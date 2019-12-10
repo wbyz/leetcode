7c72a1ca0dfcdef006e7e381680001e4e88a991d
2758 <witzel.wang@jollycorp.com>
2019-12-10 10:42:21 +0800

    a07

--- src/com/wby/leetcode/problem/a07_reverse_integer.java
+++ src/com/wby/leetcode/problem/a07_reverse_integer.java
@@ -38,19 +38,15 @@ package com.wby.leetcode.problem;
 public class a07_reverse_integer {
 
     private static int reverse(int x) {
-
-        int ans = 0;
+        int rev = 0;
         while (x != 0) {
             int pop = x % 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
-                return 0;
-            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
-                return 0;
-            ans = ans * 10 + pop;
             x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
         }
-        return ans;
-
+        return rev;
     }
 
     public static void main(String[] args) {
